d4a58323cbb4982d6c7941f4be54c0cccb76fcd8
Sue Mu <mushuyun@yahoo.com>
2020-06-29 21:06:03 -0400

    adding build

---
+++ client/build/precache-manifest.47b3d2e604e471486203a8c03a647e0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7363178d261af315593ba13563e9d21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd759c3ad11776e4b83e",
+    "url": "/static/css/2.db6f3515.chunk.css"
+  },
+  {
+    "revision": "3857a8ac1efc0401ebf5",
+    "url": "/static/css/main.47fa1b7c.chunk.css"
+  },
+  {
+    "revision": "fd759c3ad11776e4b83e",
+    "url": "/static/js/2.e786fb88.chunk.js"
+  },
+  {
+    "revision": "14060d5403b5cb599a71704f7b391b06",
+    "url": "/static/js/2.e786fb88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3857a8ac1efc0401ebf5",
+    "url": "/static/js/main.6bb054bd.chunk.js"
+  },
+  {
+    "revision": "6eb88f7ac6aaacd8acf8",
+    "url": "/static/js/runtime-main.520b8060.js"
+  }
+]);
